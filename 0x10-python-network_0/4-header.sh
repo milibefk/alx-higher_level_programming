030dc8a9f97e0409a8329d86d9889f97ea8b89c6
milibefk <99682095+milibefk@users.noreply.github.com>
2022-09-22 21:50:33 +0300

    T

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# this curl command sends a custom header
-curl -sH "X-School-User-Id:98" $1
+# Sends a GET rqst and displays the body of the response
+curl -sH "X-School-User-Id: 98" "$1"
